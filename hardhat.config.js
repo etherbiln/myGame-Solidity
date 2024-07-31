2c03e16370bd2887c2a061d6f2984163a0a16a62
Haylas <104948588+etherbiln@users.noreply.github.com>
2024-08-01 00:27:22 +0300

    Arbitrum  Integration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
+
 module.exports = {
   solidity: "0.8.20",
   networks: {
@@ -8,8 +9,12 @@ module.exports = {
       url: process.env.INFURA_SEPOLIA_URL,
       accounts: [process.env.PRIVATE_KEY],
     },
-      hardhat: {
-        chainId: 1337
-      },
+    arbitrum: {
+      url: process.env.ARBITRUM_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    hardhat: {
+      chainId: 1337,
+    },
   },
 };
