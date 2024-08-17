491e31dcfc67c45432d1321613f63121c5a80526
Haylas <104948588+etherbiln@users.noreply.github.com>
2024-08-17 20:51:13 +0300

    Uptade Contracts

--- contracts/TokenManager.sol
+++ contracts/TokenManager.sol
@@ -13,7 +13,9 @@ contract TokenManager {
     address public ownerGameAddress = 0xAC0775EA1214Dd83c9e9951e6C476605d11ECEF6;
     uint256 public constant TREASURE_REWARD = 5000 * 10**18;
     uint256 public constant SUPPORT_PACKAGE_REWARD = 500 * 10**18;
-    uint256 public constant MAX_REWARD_LIMIT = 50000;
+    uint256 public constant MAX_REWARD_LIMIT = 50000 * 10**18;
+
+    address public newOwner;
 
     // Events
     event TreasureClaimed(address indexed player, uint256 reward);
@@ -29,8 +31,9 @@ contract TokenManager {
     // Claims
     function claimTreasure(address _player) external {
         require(_player != address(0), "Invalid player address");
-        require(SUPPORT_PACKAGE_REWARD <= MAX_REWARD_LIMIT, "Support package reward exceeds limit");
+        require(blockManager.checkTreasure(_player),"You are not egilible");
         require(token.balanceOf(address(this)) >= TREASURE_REWARD, "Not enough tokens in contract");
+        require(SUPPORT_PACKAGE_REWARD <= MAX_REWARD_LIMIT, "Support package reward exceeds limit");
         require(token.transfer(_player, TREASURE_REWARD), "Treasure reward transfer failed");
 
         emit TreasureClaimed(_player, TREASURE_REWARD); // Emit event
@@ -38,9 +41,32 @@ contract TokenManager {
 
     function claimSupportPackage(address _player) external {
         require(_player != address(0), "Invalid player address");
+        require(blockManager.checkSupportPackage(_player),"You are not egilible");
         require(token.balanceOf(address(this)) >= SUPPORT_PACKAGE_REWARD, "Not enough tokens in contract");
         require(token.transfer(_player, SUPPORT_PACKAGE_REWARD), "Support package reward transfer failed");
 
         emit SupportPackageClaimed(_player, SUPPORT_PACKAGE_REWARD); // Emit event
     }
+
+    function withdrawToken() public onlyOwner {
+        uint256 tokenBalance = token.balanceOf(address(this));
+        require(tokenBalance > 0, "No tokens to withdraw"); 
+        require(token.transfer(ownerGameAddress, tokenBalance), "Token transfer failed");
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+
+    function acceptOwnership() public {
+        require(msg.sender == newOwner, "Not the new owner");
+        ownerGameAddress = newOwner;
+        newOwner = address(0);
+    }
+
+    modifier onlyOwner {
+        require(ownerGameAddress ==msg.sender);
+        _;
+    }
+
 }
